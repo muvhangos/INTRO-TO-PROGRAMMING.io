010296071a2b1fc1bab93b468d262753fa6a03ea
Samuel Muvhango <93419130+muvhangos@users.noreply.github.com>
2024-12-19 16:48:49 +0200

    Add files via upload

---
+++ Projets/dream car.js
@@ -0,0 +1,15 @@
+var dreamCar = {
+    make: "Oldsmobile",
+    model: "1980",
+    color: "brown",
+    year: 1975,
+    bodyStyle: "Luxury Car",
+    price: 500 000.00
+   };
+
+   alert("The type of dreamCar is: " + typeof dreamCar);
+
+// document.getElementById("pricetag").innerHTML = dreamCar.price;
+// document.getElementById("modelyear").innerHTML = dreamCar.year;
+// document.getElementById("body").style.backgroundColor = dreamCar.color;
+// document.getElementById("body").innerHTML = dreamCar.make + " " + dreamCar.model;
